c4d3c27ae03bd0520ecd9c368fb6342dc6ee322d
when <knowhen@outlook.com>
2021-01-21 21:41:49 +0800

    Init new project of classic java examples.

---
+++ src/test/java/org/when/example/AppTest.java
@@ -0,0 +1,19 @@
+package org.when.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ * @author when
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
